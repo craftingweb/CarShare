3c0399b7add3cbed4b1e82c21fffb2e006802b39
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-10-16 21:05:16 -0400

    reviews section

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ $(function () {
     arrows: false,
     dots: true,
     fade: true,
-    // pagination: true,
+  });
+
+  $(".reviews__slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
   });
 });
