4cf68ddba88fd6151837dc853b9a646cb1b152f8
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-10-18 17:16:58 -0400

    main & footer mobile friendly

--- js/main.js
+++ js/main.js
@@ -18,6 +18,26 @@ $(function () {
     dots: true,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1140,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
   $(".menu__btn").on("click", function () {
     $(".menu__list").toggleClass("menu__list--active");
